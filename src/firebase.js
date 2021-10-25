f16685a4a84267ac3ffcb8405784151e6c3f5b4a
Olive <oliverachermann23@gmail.com>
2021-10-25 20:49:43 +0200

    Basic sign up form completed

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+export default app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId:process.env.REACT_APP_MEASUREMENT_ID
+})
+
+export const auth = app.auth()

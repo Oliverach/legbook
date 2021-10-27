c68611b73c17a8392a3e9d9a79ddc1756c83b4b0
Olive <oliverachermann23@gmail.com>
2021-10-27 21:16:53 +0200

    dependency adjusted for user sign up

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,15 @@
-import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import firebase from "firebase/app"
+import "firebase/auth"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAEzBgWiLUfBX63E75161LRE5pccalGvXk",
-    authDomain: "legbook-336d0.firebaseapp.com",
-    projectId: "legbook-336d0",
-    storageBucket: "legbook-336d0.appspot.com",
-    messagingSenderId: "690134316072",
-    appId: "1:690134316072:web:a71e71a3fd9d579afa311d",
-    measurementId: "G-C9E932Z291"
-};
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyAEzBgWiLUfBX63E75161LRE5pccalGvXk",
+  authDomain: "legbook-336d0.firebaseapp.com",
+  projectId: "legbook-336d0",
+  storageBucket: "legbook-336d0.appspot.com",
+  messagingSenderId: "690134316072",
+  appId: "1:690134316072:web:a71e71a3fd9d579afa311d",
+  measurementId: "G-C9E932Z291"
+})
 
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app)
-
+export const auth = app.auth()
 export default app

b264dd5d88a8d4ec80f9c5ec1d4aea9b25b42987
Olive <oliverachermann23@gmail.com>
2021-12-11 13:27:27 +0100

    comment overflow auto

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState, useRef } from 'react'
 import app from 'firebase'
 import { useAuth } from "../context/AuthContext"
+import Swal from 'sweetalert2'
 
 
 export default function Dashboard() {
     const [comments, SetComments] = useState([])
     const [posts, setPosts] = useState([])
     const [openedPost, setOpenedPost] = useState()
-    const modalRef = useRef()
     const { currentUser } = useAuth()
+    const modalRef = useRef()
     const db = app.firestore()
-
-
 
     useEffect(() => {
 
@@ -25,7 +24,6 @@ export default function Dashboard() {
             setPosts(postsCollection.docs.map(doc => {
                 return { docId: doc.id, data: doc.data(), username: "fix this" }
             }))
-
         }
         fetchPosts()
     }, [])
@@ -37,18 +35,22 @@ export default function Dashboard() {
             post: docId,
             user: uid
         }).then(function () {
-            console.log("Comment successfully written!")
+            Swal.fire(
+                'Commented!',
+                '',
+                'success'
+            )
         }).catch(function (error) {
             console.error("Error commenting: ", error)
         })
     }
 
-    const fetchComments = async (postId) =>{
+    const fetchComments = async (postId) => {
         const commentCollection = await db.collection('comments').where('post', '==', postId).get()
         SetComments(commentCollection.docs.map(doc => {
             return { comment: doc.data().comment, username: "fix this" }
         }))
-        
+
     }
 
     return (
@@ -58,38 +60,38 @@ export default function Dashboard() {
             <div className="modal">
                 <div className="modal-box">
                     {openedPost && <img src={openedPost.data.fileUrl} className="mx-auto" style={{ objectFit: "cover" }} alt={openedPost.description} />}
-
-                    <div class="collapse mx-auto border rounded-box border-base-300 collapse-arrow">
+                    <div class="collapse rounded-2xl mx-auto mt-2 collapse-arrow  ">
                         <input type="checkbox" />
-                        <div class="collapse-title text-xl font-medium">
-                            View all comment
-                        </div>
+                        <h5 class="collapse-title font-bold">
+                            View comments
+                        </h5>
                         <div class="collapse-content">
-                            {comments.map((comment)=>{
-                             return(   <p>{comment.comment}</p>)
-                            })}
+                            {comments.length ? (
+                                    comments.map((comment) => {
+                                        return <p>fix this:{comment.comment}</p>
+                                    })) : (<h6>No comments yet</h6>)}
                         </div>
                     </div>
                     <div className="modal-action">
-                        <button type="submit" className="btn btn-primary" >Comment</button>
                         <label htmlFor="openedPost" className="btn">Close</label>
                     </div>
                 </div>
             </div>
 
-
             <div className="flex flex-col">
                 {posts.map((post) => {
                     return (
 
-                        <div className="card w-1/4 mx-auto my-10 cursor-pointer shadow-2xl" key={post.data.description} >
+                        <div className="card w-1/3 mx-auto my-10 cursor-pointer shadow-2xl" key={post.data.description} >
                             <figure>
-                                <img src={post.data.fileUrl} style={{ objectFit: "cover" }} alt={post.data.description} onClick={(e) => {
-                                    e.preventDefault()
-                                    fetchComments(post.docId)
-                                    setOpenedPost(post)
-                                    modalRef.current.click()
-                                }} />
+                                <label htmlFor="openedPost">
+                                    <img src={post.data.fileUrl} style={{ objectFit: "cover" }} alt={post.data.description} onClick={(e) => {
+                                        e.preventDefault()
+                                        fetchComments(post.docId)
+                                        setOpenedPost(post)
+                                        modalRef.current.click()
+                                    }} />
+                                </label>
                             </figure>
                             <div className="card-body">
 
@@ -102,6 +104,7 @@ export default function Dashboard() {
                                     <form onSubmit={(e) => {
                                         e.preventDefault()
                                         commentPost(e.target.comment.value, post.docId, currentUser.uid)
+                                        e.target.comment.value = ""
                                     }}>
                                         <input type="text" placeholder="Leave a comment" name="comment" className="w-full pr-16 rounded-t-none rounded-b-2xl input input-bordered" />
                                         <button type="submit" className="absolute top-0 right-0 rounded-l-none btn rounded-t-none rounded-b-2xl">
